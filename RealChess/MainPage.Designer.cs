1b8c93b0b55bd150ab94e9ee4e6c958a7fb00b60
Endieym <endieym2@gmail.com>
2023-02-06 20:46:16 +0200

    Added picture to the main page, and changed background color

--- RealChess/MainPage.Designer.cs
+++ RealChess/MainPage.Designer.cs
@@ -28,15 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(310, 159);
+            this.button1.Location = new System.Drawing.Point(305, 147);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(186, 25);
             this.button1.TabIndex = 0;
@@ -46,16 +51,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(365, 44);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.Size = new System.Drawing.Size(133, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "REALCHESS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(307, 213);
+            this.button2.Location = new System.Drawing.Point(305, 216);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(186, 25);
             this.button2.TabIndex = 2;
@@ -64,24 +71,51 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(310, 273);
+            this.button3.Location = new System.Drawing.Point(305, 285);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(186, 25);
             this.button3.TabIndex = 3;
             this.button3.Text = "RULES";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::RealChess.Properties.Resources.realchessIcon;
+            this.pictureBox1.InitialImage = global::RealChess.Properties.Resources.realchessIcon;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(171)))), ((int)(((byte)(34)))));
             this.ClientSize = new System.Drawing.Size(837, 492);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainPage";
+            this.Text = "   ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +127,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- RealChess/MainPage.cs
+++ RealChess/MainPage.cs
@@ -16,5 +16,10 @@ namespace RealChess
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
