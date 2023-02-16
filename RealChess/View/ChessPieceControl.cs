0f390d37181ea23cd64906db2bbac68f28cfe470
Endieym <endieym2@gmail.com>
2023-02-16 18:53:33 +0200

    Added a usercontrol for chess pieces

---
+++ RealChess/View/ChessPieceControl.cs
@@ -0,0 +1,36 @@
+﻿using RealChess.View.Forms.BoardGraphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealChess.View
+{
+    public partial class ChessPieceControl : UserControl
+    {
+        public ChessPiece Piece { get; set; }
+
+        public ChessPieceControl()
+        {
+            InitializeComponent();
+        }
+        public void SetPiece(ChessPiece piece)
+        {
+            Piece = piece;
+            pictureBox1.Image = LoadPieceImage(piece);
+        }
+
+        private Image LoadPieceImage(ChessPiece piece)
+        {
+            string imageName = $"{piece.Color.ToString().ToLower()}_{piece.Type.ToString().ToLower()}.png";
+            return Properties.Resources.ResourceManager.GetObject(imageName) as Image;
+
+            
+        }
+    }
+}
---
+++ RealChess/View/Forms/BoardGraphics/ChessPiece.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealChess.View.Forms.BoardGraphics
+{
+    public class ChessPiece
+    {
+        public ChessPiece()
+        {
+        }
+
+        public enum PieceColor { WHITE, BLACK }
+        public enum PieceType { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING }
+
+        public PieceColor Color{ get; set; }
+        public PieceType Type{ get; set; }
+        
+    
+}
+    }
